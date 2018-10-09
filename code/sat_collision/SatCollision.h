fad40a61a1fe00cb709c74d6de0346b528246162
Mr Grey <ksmaybe@gmail.com>
2018-10-09 16:02:36 -0400

    delete

--- code/sat_collision/SatCollision.h
+++
@@ -1,5 +0,0 @@
-
-#include <vector>
-#include <utility>
-
-bool CheckSATCollision(const std::vector<std::pair<float,float>> &e1Points, const std::vector<std::pair<float,float>> &e2Points, std::pair<float,float> &penetration);
